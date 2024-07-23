7455003953adcb1a1113112c5ecac743ab05dfa4
Christos Rentzos <rentzosc@gmail.com>
2024-07-23 20:49:24 +0300

    added review entity and repository

--- src/main/java/com/luv2code/spring_boot_library/config/MyDataRestConfig.java
+++ src/main/java/com/luv2code/spring_boot_library/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.luv2code.spring_boot_library.config;
 
 import com.luv2code.spring_boot_library.entity.Book;
+import com.luv2code.spring_boot_library.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -22,8 +23,10 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
 
         config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
 
         disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
 
         /* Configure CORS Mapping */
         /* Had to mannually set all headers to be allowed */
