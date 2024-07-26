362bf521f66602d62025cd16dba7a17a42f1cfc4
Christos Rentzos <rentzosc@gmail.com>
2024-07-27 00:56:59 +0300

    review and checkout

--- src/main/java/com/luv2code/spring_boot_library/controller/BookController.java
+++ src/main/java/com/luv2code/spring_boot_library/controller/BookController.java
@@ -6,6 +6,8 @@ import com.luv2code.spring_boot_library.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
+
 @CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/api/books")
@@ -19,23 +21,25 @@ public class BookController {
         this.bookService = bookService;
     }
 
+
     @GetMapping("/secure/currentloans/count")
     public int currentLoansCount(@RequestHeader(value = "Authorization") String token) {
-        String userEmail = "testuser@email.com";
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.currentLoansCount(userEmail);
     }
 
     @GetMapping("/secure/ischeckedout/byuser")
     public Boolean checkoutBookByUser(@RequestHeader(value = "Authorization") String token,
                                       @RequestParam Long bookId) {
-        String userEmail = ExtractJWT.payloadJWTExtraction(token);
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBookByUser(userEmail, bookId);
+
     }
 
     @PutMapping("/secure/checkout")
     public Book checkoutBook(@RequestHeader(value = "Authorization") String token,
                              @RequestParam Long bookId) throws Exception {
-        String userEmail = "testuser@email.com";
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBook(userEmail, bookId);
     }
 }
