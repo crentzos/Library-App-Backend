21935c1ba847487ee117487ba2be69566706eb3c
Christos Rentzos <rentzosc@gmail.com>
2024-08-03 16:13:46 +0300

    submit admin response to user question

--- src/main/java/com/luv2code/spring_boot_library/controller/MessagesController.java
+++ src/main/java/com/luv2code/spring_boot_library/controller/MessagesController.java
@@ -1,5 +1,6 @@
 package com.luv2code.spring_boot_library.controller;
 
+import com.luv2code.spring_boot_library.entity.AdminQuestionRequest;
 import com.luv2code.spring_boot_library.entity.Message;
 import com.luv2code.spring_boot_library.service.MessagesService;
 import com.luv2code.spring_boot_library.utils.ExtractJWT;
@@ -23,5 +24,16 @@ public class MessagesController {
                             @RequestBody Message messageRequest) {
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         messagesService.postMessage(messageRequest, userEmail);
+    }
+
+    @PutMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader(value = "Authorization") String token,
+                           @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only.");
+        }
+        messagesService.putMessage(adminQuestionRequest, userEmail);
     }
 }
--- src/main/java/com/luv2code/spring_boot_library/entity/Message.java
+++ src/main/java/com/luv2code/spring_boot_library/entity/Message.java
@@ -32,7 +32,7 @@ public class Message {
     private String response;
 
     @Column(name = "closed")
-    private Boolean closed;
+    private boolean closed;
 
 
     public Message() {}
