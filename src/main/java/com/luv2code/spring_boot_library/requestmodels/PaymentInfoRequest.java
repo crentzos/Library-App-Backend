819563d80787a8aa43fb1eb62d54de0b9e5ff9c1
Christos Rentzos <rentzosc@gmail.com>
2024-08-07 00:29:23 +0300

    App completed

---
+++ src/main/java/com/luv2code/spring_boot_library/requestmodels/PaymentInfoRequest.java
@@ -0,0 +1,13 @@
+package com.luv2code.spring_boot_library.requestmodels;
+
+import lombok.Data;
+
+@Data
+public class PaymentInfoRequest {
+
+    private int amount;
+
+    private String currency;
+
+    private String receiptEmail;
+}
