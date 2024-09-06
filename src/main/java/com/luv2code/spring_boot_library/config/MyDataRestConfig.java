0958dd364e676f7cd48d56c5f4812374763cb9cc
Christos Rentzos <rentzosc@gmail.com>
2024-09-06 21:34:17 +0300

    refactored cors to talk with front end

--- src/main/java/com/luv2code/spring_boot_library/config/MyDataRestConfig.java
+++ src/main/java/com/luv2code/spring_boot_library/config/MyDataRestConfig.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
     private final String[] allowedOrigins = {
-            "https://graceful-trifle-6c3656.netlify.app",
+            "https://library-app-frontend.onrender.com",
             "https://localhost:3000"
     };
 
