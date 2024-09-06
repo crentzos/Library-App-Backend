a8666c032bb4d2e115e612801cd11baf83af61d2
Christos Rentzos <rentzosc@gmail.com>
2024-09-07 00:02:14 +0300

    refactored security

--- src/main/java/com/luv2code/spring_boot_library/config/SecurityConfiguration.java
+++ src/main/java/com/luv2code/spring_boot_library/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ package com.luv2code.spring_boot_library.config;
 import com.okta.spring.boot.oauth.Okta;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.accept.ContentNegotiationStrategy;
@@ -21,27 +22,30 @@ public class SecurityConfiguration {
         // Disable Cross Site Request Forgery
         http.csrf().disable();
 
-        // Protect endpoints at /api/<type>/secure
-        http.authorizeRequests(configurer ->
-                        configurer
-                                .antMatchers("/api/books/secure/**", "/api/reviews/secure/**",
-                                        "/api/messages/secure/**", "/api/admin/secure/**")
-                                .authenticated())
-                        .oauth2ResourceServer()
-                        .jwt();
-
-        // Add CORS filters
+        // Allow CORS and disable CSRF
         http.cors();
+
+        // Allow OPTIONS requests to bypass security
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()  // Allow all preflight CORS requests
+                .antMatchers("/api/books/secure/**", "/api/reviews/secure/**",
+                        "/api/messages/secure/**", "/api/admin/secure/**")
+                .authenticated()
+                .anyRequest().permitAll();
+
+        // Set up OAuth2 Resource Server to authenticate requests via JWT
+        http.oauth2ResourceServer()
+                .jwt();
 
         // Add content negotiation strategy
         http.setSharedObject(ContentNegotiationStrategy.class,
                 new HeaderContentNegotiationStrategy());
 
-
         // Force a non-empty response body for 401's to make the response friendly
         Okta.configureResourceServer401ResponseBody(http);
-
 
         return http.build();
     }
 }
+
+
